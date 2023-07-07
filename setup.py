bf31aa94754e128c005f0314125ccb1a6b8eba84
Trevor Maseleme <tkmaseleme@gmail.com>
2023-07-07 09:50:12 +0200

    Anansi package created

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setuptools.setup(
     version='1.0.0',
     author='Trevor Maseleme',
     author_email='tkmaseleme@gmail.com',
-    description='Package to allow you to simulate the game of black jack in your code',
+    description='Package to allow you to include several card and kids games in your code',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/tkmaseleme/anansi',
     project_urls = {
         "Bug Tracker": "https://github.com/tkmaseleme/anansi/issues"
     },
-    license='MIT',
-    packages=['blackjack', 'high_low'],
-    install_requires=[''],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Os Independent"
+    ]
 )
